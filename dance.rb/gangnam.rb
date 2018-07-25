057816475ca372de388a19f4ac1fd4377cb1e94b
nicolettabriscoe <nicolettaskye@icloud.com>
2018-07-25 22:47:54 +0000

    automatically backed up by learn

---
+++ dance.rb/gangnam.rb
@@ -0,0 +1,31 @@
+def lower body
+  puts "Plant legs far apart, bend knees"
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "small skip step backwards"
+end 
+def skip step left 
+  puts "lower left foot to ground"
+  puts "bounce left foot back up slightly, kicking it a few inches back"
+  puts "left, right, left, and left"
+end 
+def skip step right 
+  puts "lower right foot to ground"
+  puts "bounce right foot back up slightly, kicking a few inches back"
+  puts "right left, right and right"
+end 
+def bob the reigns 
+  puts "holding arms out infront of you straight and at chest level"
+  puts "cross right wrist over left, hold them together"
+  puts "lift your arms up and down in a loose bouncing movement, movement is repeated eight times"
+end 
+def lasso 
+  puts "hold left arm so back of your wrist is near your chin, pointing your left elbow straight left"
+  puts "lift right arm up and point diagonally to the right"
+  puts "raise right forearm so that it points straight up, whip it in a small circle, movement is repeated eight times"
+end 
+
+bob the reigns 
+skip step right
+bob the reigns 
+skip step left 
